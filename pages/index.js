f406be1caf07cb8372104e9c566e2a66ed9748d4
schhunheng <schhunheng.it@gmail.com>
2021-07-14 15:51:18 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function conditional(){
        
             <div>
                 <Head>
-                    <title>Sabaicode</title>
+                    <title>Sabaicode Update </title>
                 </Head>
                 <Navbar />
                 <div className={styled.fContainer}>
